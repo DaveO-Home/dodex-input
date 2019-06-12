af6ff02cc4f2fd983ba11299685872e11d942d97
DaveO <dufferdo2@gmail.com>
2019-06-12 14:50:14 -0700

    Dodex Front-End content loader

---
+++ data/content.js
@@ -0,0 +1,95 @@
+
+ var dodexContent = {
+	cards: {
+		card1: {
+			tab: "A",
+			front: {
+				content: `<p>Ace's Info - (123)555-5567</p>`
+			},
+			back: {
+				content: ""
+			}
+		},
+		card2: {
+			tab: "B",
+			front: {
+				"content": ""
+			},
+			back: {
+				content: ""
+			}
+		},
+		card3: {
+			tab: "C",
+			front: {
+				"content": `<h1>Best's Contact Form</h1><a href="#!contact"><i class="fa fa-fw fa-phone"></i>Contact</a>`
+			},
+			back: {
+				content: `<h1>Lorem Ipsum</h1><a href="https://www.yahoo.com" target="_">Yahoo</a>`
+			}
+		},
+		card4: {
+			tab: "D",
+			front: {
+				content: ""
+			},
+			back: {
+				content: ""
+			}
+		},
+		card5: {
+			tab: "E",
+			front: {
+				content: ""
+			},
+			back: {
+				content: ""
+			}
+		},
+		card6: {
+			tab: "F",
+			front: {
+				content: ""
+			},
+			back: {
+				content: ""
+			}
+		},
+		"card16": {
+			tab: "P",
+			front: {
+				content: "<h1>Test Pdf</h1><a href=\"#!pdf/test\"><i class=\"fa fa-fw fa-file-pdf-o\"></i>PDF View</a>"
+			},
+			back: {
+				content: "<h1>Lorem Ipsum</h1><a href=\"https://www.yahoo.com\" target=\"_\">Yahoo16</a>"
+			}
+		},
+		card20: {
+			tab: "T",
+			front: {
+				content: "<h1>Test Table</h1><a href=\"#!table/tools\"><i class=\"fa fa-fw fa-table\"></i>Table View</a>"
+			},
+			back: {
+				content: "<h1>Lorem Ipsum</h1><a href=\"https://www.yahoo.com\" target=\"_\">Yahoo20</a>"
+			}
+		},
+		card8: {
+			tab: "H",
+			front: {
+				content: "<h1>Description</h1><a href=\"#!\"><i class=\"fa fa-fw fa-home\"></i>Home</a>"
+			},
+			back: {
+				content: "<h1>Lorem Ipsum</h1><a href=\"https://www.yahoo.com\" target=\"_\">Yahoo8</a>"
+			}
+		},
+		card27: {
+			tab: "",
+			front: {
+				content: ""
+			},
+			back: {
+				content: "<p>Hi - from <h2>Dodex<h2></p>"
+			}
+		}
+	}
+}
